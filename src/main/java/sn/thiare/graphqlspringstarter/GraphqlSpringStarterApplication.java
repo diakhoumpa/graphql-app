fded3d246cf22a9db9235610453d78dca8fadddc
thiareMohamed <thiaremohamed.mt@gmail.com>
2024-02-29 12:32:45 +0000

    load and configure databbase h2

--- src/main/java/sn/thiare/graphqlspringstarter/GraphqlSpringStarterApplication.java
+++ src/main/java/sn/thiare/graphqlspringstarter/GraphqlSpringStarterApplication.java
@@ -1,7 +1,13 @@
 package sn.thiare.graphqlspringstarter;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import sn.thiare.graphqlspringstarter.entities.Student;
+import sn.thiare.graphqlspringstarter.repository.StudentRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class GraphqlSpringStarterApplication {
@@ -10,4 +16,11 @@ public class GraphqlSpringStarterApplication {
         SpringApplication.run(GraphqlSpringStarterApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(StudentRepository studentRepository) {
+        return args -> {
+        List.of("Thiare", "Mamadou", "Moussa", "Fatou", "Awa").forEach(name -> studentRepository.save(new Student(null, name, name + "@gmail.com", "77 123 45 67")));
+            studentRepository.findAll().forEach(System.out::println);
+        };
+    }
 }
