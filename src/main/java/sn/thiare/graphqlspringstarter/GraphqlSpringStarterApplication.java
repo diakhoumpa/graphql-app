8c024d9a563b8234a9b0150ab3c49a0daa10431d
thiareMohamed <thiaremohamed.mt@gmail.com>
2024-02-29 12:12:53 +0000

    initial commit

---
+++ src/main/java/sn/thiare/graphqlspringstarter/GraphqlSpringStarterApplication.java
@@ -0,0 +1,13 @@
+package sn.thiare.graphqlspringstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlSpringStarterApplication.class, args);
+    }
+
+}
