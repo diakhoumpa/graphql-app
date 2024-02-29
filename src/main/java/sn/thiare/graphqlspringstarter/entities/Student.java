1c9c5b78812bc186d31ddc01a5a02321f3cdf6ff
thiareMohamed <thiaremohamed.mt@gmail.com>
2024-02-29 12:13:08 +0000

    add entity student

---
+++ src/main/java/sn/thiare/graphqlspringstarter/entities/Student.java
@@ -0,0 +1,24 @@
+package sn.thiare.graphqlspringstarter.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+}
